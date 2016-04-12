da1d9b368e988f82b4ba6932367d969ea0c394db
Adam Robertson <adam@arcreative.net>
2016-04-12 12:22:12 -0700

    Added tries and tryInterval

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ module.exports = function(homebridge){
 
 function XboxAccessory(log, config) {
   this.log = log;
-  this.name = config["name"] || 'Xbox';
-  this.xbox = new Xbox(config["ipAddress"], config["liveId"]);
+  this.name = config['name'] || 'Xbox';
+  this.xbox = new Xbox(config['ipAddress'], config['liveId']);
+  this.tries = config['tries'] || 5;
+  this.tryInterval = config['tryInterval'] || 1000;
 }
 
 XboxAccessory.prototype = {
 
   setPowerState: function(powerOn, callback) {
     this.log("Sending on command to '" + this.name + "'...");
-    this.xbox.powerOn(callback);
+
+    for (var i = 0; i < this.tries; i++) {
+
+      // Queue tries times at tryInterval
+      setTimeout(function() {
+        this.xbox.powerOn();
+      }, i * this.tryInterval);
+
+      // Don't really care about powerOn errors, and don't want more than one callback
+      callback();
+    }
   },
 
   identify: function(callback) {
